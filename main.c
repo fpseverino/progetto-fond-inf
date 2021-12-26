0016eb309dee484770350a97568769732e989e5f
Francesco Paolo Severino <francescopaolo.severino@gmail.com>
2021-12-26 16:28:29 +0100

    Pulizia main.c

--- main.c
+++ main.c
@@ -2,12 +2,12 @@
 //  main.c
 //  progettoFondInf
 //
-//  Created by Francesco Paolo Severino and Roberto Giovanni Scolari on 25/12/21.
+//  Created by Francesco Paolo Severino and Roberto Giovanni Scolari on 26/12/21.
 //
 
 #include <stdio.h>
 
 int main() {
-    printf("Robbettoooooooo!!!!!\n");
+    printf("Hello, World!\n");
     return 0;
 }
